a5e3daf2eaa4559aaf233e3ed85bf65d47f23126
Mixail Maksimov <maks.misha2001@gmail.com>
2024-04-29 09:31:07 +0300

    Task1

---
+++ project_tracker/quality_control/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+from tasks.models import Task, Project
+
+
+class BugReport(models.Model):
+    STATUS_CHOICES = [
+        ('New', 'Новая'),
+        ('In_progress', 'В работе'),
+        ('Completed', 'Завершена'),
+    ]
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE
+    )
+    task = models.ForeignKey(
+        Task,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    status = models.CharField(
+        max_length=50,
+        choices=STATUS_CHOICES,
+        default='New'
+    )
+    priority = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class FeatureRequest(models.Model):
+    STATUS_CHOICES = [
+        ('Handling', 'Рассмотрение'),
+        ('Accepted', 'Принято'),
+        ('Rejected', 'Отклонено'),
+    ]
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE
+    )
+    task = models.ForeignKey(
+        Task,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    status = models.CharField(
+        max_length=50,
+        choices=STATUS_CHOICES,
+        default='Handling'
+    )
+    priority = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
